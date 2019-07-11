d97ffd2c26af299e9722e9b296304e3794ab0daa
LX-LXF <2397784999@qq.com>
2019-07-11 16:44:27 +0800

    表单校验完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,19 @@
   <div class="login-coment">
     <el-card class="login-box">
       <img src="../../assets/images/logo_index.png" alt />
-      <el-form>
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form status-icon ref="loginForm" :model="loginForm" :rules="loginRulse">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input placeholder="请输入验证码" style="width:280px"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:280px"></el-input>
           <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
         <el-checkbox v-model="checked"></el-checkbox>我已阅读并同意
         <el-link type="primary" :underline="false">用户协议</el-link>和
         <el-link type="primary" :underline="false">隐私条款</el-link>
         <el-form-item>
-          <el-button  class="boxx" type="primary">登 录</el-button>
+          <el-button class="boxx" type="primary" @click="login()">登 录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -24,11 +24,41 @@
 <script>
 export default {
   data() {
-    return {
-      checked : true
-      
+    const checkMobile = (rule, value, callback) =>{
+      console.log(123123)
+      if(/^1[3-9]\d{9}$/.test(value)) {
+         callback()
+      } else {
+        callback(new Error('手机号格式不对'))
+      }
     }
+    return {
+      checked: true,
+
+      loginForm: {
+        mobile: "",
+        code: ""
+      },
+      loginRulse: {
+        mobile: [
+          { required: true, message: "给我把手机号填上", trigger: "blur" },
+          {validator:checkMobile, trigger: "blur" },
+        ],
+        code: [
+          { required: true, message: "给我把验证码填上", trigger: "blur" },
+          { len: 6, message: '验证码是6位', trigger: 'blur' }
+        ]
+      }
+    };
   },
+  methods: {
+    login() {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+        }
+      });
+    }
+  }
 };
 </script>
 
@@ -52,9 +82,9 @@ export default {
       width: 200px;
       margin: 10px auto 30px;
     }
-    .boxx{
-        width: 100%;
-        margin-top: 30px;
+    .boxx {
+      width: 100%;
+      margin-top: 30px;
     }
   }
 }
